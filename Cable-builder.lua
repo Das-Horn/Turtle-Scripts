c0aacf3616052a001f17137a9010752d2b43d212
yolo12345678945 <32498304+yolo12345678945@users.noreply.github.com>
2019-12-04 09:28:08 +0000

    Started to create a turtle that can lay cable
    Designed to lay Cable across long distances

---
+++ Cable-builder.lua
@@ -0,0 +1,19 @@
+local Hx, Hy, Hz = 0, 0, 0
+local Cx, Cy, Cz = 0, 0, 0
+local Dx, Dy, Dz = 0, 0, 0
+local Cdir = 0
+local Ldir = 0
+
+-- Get Destination Co-ords
+
+print("Welcome The Home co-ordinantes have been set. To continue you will have to input the RELATIVE Co-ords of the destination compared to the Co-ords the turtle's HOME co-ordinantes.")
+print("You will be prompted to enter a value three times. These are the RELATIVE Destination Co-ords")
+
+-- Navigation
+
+-- Construction
+
+-- Checks
+
+-- Misc Functions
+
